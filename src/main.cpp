614e6d6bd51e3826129cc32e731c3122fd623d4c
ImpendingMoon <theimpendingmoon@gmail.com>
2023-07-15 12:01:43 -0500

    Add mainInit and mainExit
    Additions:
    +main.hpp
    +mainInit()
    +mainExit()

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,45 @@
  @file main.cpp
  @brief Handles program initialization
  @author ImpendingMoon
- @date 2023-07-14
+ @date 2023-07-15
  */
 
-
+#include "main.hpp"
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
 
 int main(int argc, char** argv)
 {
-    return 0;
+	mainInit();
+	mainExit();
+	return 0;
+}
+
+
+
+/**
+ * @brief Initializes SDL, Logger, and the Window.
+ * @return 0 on success, 1 on SDL error
+ */
+int mainInit(void)
+{
+	int err;
+	err = SDL_Init(
+		0
+		| SDL_INIT_VIDEO
+		| SDL_INIT_EVENTS
+	);
+
+	if(err != 0) { return 1; }
+	return 0;
+}
+
+
+
+/**
+ * @brief Properly quits SDL and Logger.
+ */
+void mainExit(void)
+{
+	SDL_Quit();
 }
---
+++ src/main.hpp
@@ -0,0 +1,19 @@
+/**
+ @file main.hpp
+ @brief Handles program initialization
+ @author ImpendingMoon
+ @date 2023-07-15
+ */
+
+#pragma once
+
+/**
+ * @brief Initializes SDL, Logger, and the Window.
+ * @return 0 on success, 1 on SDL error
+ */
+int mainInit(void);
+
+/**
+ * @brief Properly quits SDL and Logger.
+ */
+void mainExit(void);
